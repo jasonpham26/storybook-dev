c9b04285d55dcc900fe49948b8ccf01a7d7c4e32
Phuong Pham <vinhphuong0206@gmail.com>
2018-09-05 19:57:20 +1000

    handle auth

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
 const express = require("express");
 const mongoose = require("mongoose");
-
+const passport = require("passport");
+// Load Routes
+const auth = require("./routes/auth");
 const app = express();
 const port = process.env.PORT || 5000;
 
@@ -8,6 +10,10 @@ const port = process.env.PORT || 5000;
 app.get("/", (req, res) => {
   res.send("Worked!");
 });
+// Passport Config
+require("./config/passport")(passport);
+// Use routes
+app.use("/auth", auth);
 app.listen(port, () => {
   console.log(`Server started on port ${port}`);
 });
---
+++ config/keys.js
@@ -0,0 +1,7 @@
+// Accces Google Oauth
+module.exports = {
+  mongoURI: "mongodb://tin:1558@ds245772.mlab.com:45772/storybooks-dev",
+  googleClientID:
+    "69727381813-6er0m8tushc4mghs6s16nse1b2j01r59.apps.googleusercontent.com",
+  googleClientSecret: "W2PDdbCp9bx2a9jFej5ReiMw"
+};
